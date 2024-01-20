7a14994d05d4f04844abfd0fd688f78bbacbba01
ussenuk <ukimanuka@gmail.com>
2024-01-20 16:30:06 +0300

    understanding attributes property and setter

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def title(self):
+        return self._title
+    @property
+    def page_count(self):
+        return self._page_count
     
-        
+    @title.setter
+    def title(self, value):
+        if isinstance(value, str):
+            self._title = value
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+            # self.page_count = ""
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+Hary_potter = Book("Hary Potter", 272)
+    
+# print(Hary_potter.page_count)        
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self._condition = "New"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    @property
+    def condition(self):
+        return self._condition
+
+    @condition.setter
+    def condition(self, value):
+        self._condition = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+addis = Shoe("Addidas", 9)
+addis.condition = "New"
+print(addis.size)
